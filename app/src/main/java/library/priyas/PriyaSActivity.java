04b5fa464524513d5e4a20f355b6c3a5f1eebde2
priyashrivastava <priya.shrivastava@live.com>
2016-11-06 23:53:04 -0500

    Tut 7 start

--- app/src/main/java/library/priyas/PriyaSActivity.java
+++ app/src/main/java/library/priyas/PriyaSActivity.java
@@ -1,58 +1,47 @@
 package library.priyas;
 
-import library.priyas.constant.SQLCommand;
-import library.priyas.view.TableView;
 import library.priyas.util.DBOperator;
-//import android.app.Activity;
 import android.content.Intent;
-import android.database.Cursor;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.ScrollView;
 
 
+public class PriyaSActivity extends AppCompatActivity implements OnClickListener{
 
-public class PriyaSActivity extends AppCompatActivity {
+    Button checkoutBtn,queryBtn;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_priya_s_layout);
-
+        setContentView(R.layout. activity_priya_s_layout);
+        checkoutBtn=(Button)this.findViewById(R.id.goCheckOut_btn);
+        checkoutBtn.setOnClickListener(this);
+        queryBtn=(Button)this.findViewById(R.id.goDoQuery_btn);
+        queryBtn.setOnClickListener(this);
         //copy database file
         try{
             DBOperator.copyDB(getBaseContext());
         }catch(Exception e){
             e.printStackTrace();
         }
-        //implement SQL query and get cursor of resultset
-//            Cursor cursor = DBOperator.getInstance().execQuery(SQLCommand.QUERY_STUDENT);
-//            TableView tableView = new TableView(this, cursor);
-//            //show data in tableview
-        ScrollView scrollView = (ScrollView)this.findViewById(R.id.scrollView);
-//            scrollView.addView(tableView);
+    }
 
-        /*OnClickListener listnr=new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent i = new Intent("query_priya_s_layout");
-                Curr    entActivity.this.startActivity(myIntent);
-                startActivity(i);
-            }
-        };
-        Button goDoQuery_btn =(Button) findViewById(R.id.goDoQuery_btn);
-        goDoQuery_btn.setOnClickListener(listnr);
-        */
+    public void onClick(View v)
+    {
+        int id=v.getId();
+        if (id==R.id.goCheckOut_btn){
 
-        Button next = (Button) findViewById(R.id.goDoQuery_btn);
-        next.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                Intent myIntent = new Intent(view.getContext(), QueryActivity.class);
-                startActivityForResult(myIntent, 0);
-            }
+            Intent intent = new Intent(this, CheckoutActivity.class);
+            this.startActivity(intent);
 
-        });
+
+        }else if (id==R.id.goDoQuery_btn){
+            Intent intent = new Intent(this, QueryActivity.class);
+            this.startActivity(intent);
+        }
     }
 }
--- app/src/main/java/library/priyas/constant/SQLCommand.java
+++ app/src/main/java/library/priyas/constant/SQLCommand.java
@@ -8,6 +8,22 @@ public abstract class SQLCommand
 {
     //query all students
     public static String QUERY_STUDENT = "select stid, stname from Student";
+
+    //list all data in books table
+    public static String QUERY_1 = "select lbcallnum, lbtitle from libbook";
+    //List the call numbers of books with the title ‘Database Management’
+    public static String QUERY_2 = "select lbcallnum from libbook where lbtitle like '%Database Management%'";
+    public static String QUERY_3 = "select * from checkout";
+    public static String QUERY_4 = "select libbook.lbcallnum, checkout.stid, checkout.cofine from libbook, checkout where cofine>2 group by checkout.stid ";
+    public static String QUERY_5 = "select student.* from student, checkout where student.stid=checkout.stid";
+    public static String QUERY_6 = "select * from libbook where lbtitle like '%Organizations%'";
+    public static String QUERY_7 = "select stid, stname from student where stname like'%Jefferson%'";
+    //public static String QUERY_8 = "select lbcallnum from libbook where lbtitle like '%Contract Law%'";
+
+    public static String RETURN_BOOK = "update checkout set coreturned=? where stid=? and lbcallnum=?";
+    public static String CHECK_BOOK = "insert into checkout(stid,lbcallnum,coduedate,coreturned) values(?,?,?,?)";
+//public static String CHECKOUT_SUMMARY = "select strftime('%m',coduedate) as month,count(*) as total from checkout where strftime('%Y',coduedate)='2011' group by month order by total desc";
+//public static String CHECKOUT_LIST = "select checkout.stid as _id, lbtitle, coduedate,coreturned,cofine,stname from checkout,student,libbook where student.stid=checkout.stid and libbook.lbcallnum=checkout.lbcallnum";
 }
 
 
